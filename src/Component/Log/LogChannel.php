63363fcbdc8d3abf028b3fdd8e1ded21107259fa
Giles <giles.wang@aliyun.com>
2024-04-02 09:48:51 +0800

    fix 更新yolaile/library 包

--- src/Component/Log/LogChannel.php
+++ src/Component/Log/LogChannel.php
@@ -14,6 +14,8 @@ class LogChannel
     protected const PHP_LOGIC_EXCEPTION = 'php.logic.exception';
     protected const PHP_ERROR_EXCEPTION = 'php.error.exception';
     protected const PHP_REQUEST_PARAMS  = 'php.request.params';
+
+    protected const PHP_RESPONSE_PARAMS  = 'php.response.params';
     protected const PHP_BUSINESS_LOGIC  = 'php.business.logic';
     protected const PHP_DEBUG           = 'php.debug';
     protected const PHP_DOT             = 'php.application';
@@ -26,12 +28,13 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:09
      */
-    public static function support()
+    public static function support():string
     {
         return [
             self::PHP_ERROR_EXCEPTION,
             self::PHP_LOGIC_EXCEPTION,
             self::PHP_REQUEST_PARAMS,
+            self::PHP_RESPONSE_PARAMS,
             self::PHP_BUSINESS_LOGIC,
             self::PHP_DEBUG,
             self::PHP_DOT,
@@ -46,7 +49,7 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:17
      */
-    public static function default()
+    public static function default():string
     {
         return self::PHP_DOT;
     }
@@ -58,7 +61,7 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:11
      */
-    public static function phpLogicException()
+    public static function phpLogicException():string
     {
         return self::PHP_LOGIC_EXCEPTION;
     }
@@ -70,7 +73,7 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:11
      */
-    public static function phpErrorException()
+    public static function phpErrorException():string
     {
         return self::PHP_ERROR_EXCEPTION;
     }
@@ -82,9 +85,21 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:11
      */
-    public static function requestParams()
+    public static function requestParams():string
     {
         return self::PHP_REQUEST_PARAMS;
+    }
+
+    /**
+     * responseParams 返回参数日志 通道名称
+     *
+     * @return string
+     * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
+     * @date 2024/4/2 09:48
+     */
+    public static function responseParams():string
+    {
+        return self::PHP_RESPONSE_PARAMS;
     }
 
     /**
@@ -94,7 +109,7 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:11
      */
-    public static function businessLogic()
+    public static function businessLogic():string
     {
         return self::PHP_BUSINESS_LOGIC;
     }
@@ -106,7 +121,7 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:11
      */
-    public static function debug()
+    public static function debug():string
     {
         return self::PHP_DEBUG;
     }
@@ -118,7 +133,7 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:11
      */
-    public static function dot()
+    public static function dot():string
     {
         return self::PHP_DOT;
     }
@@ -130,7 +145,7 @@ class LogChannel
      * @author Giles <giles.wang@aliyun.com|giles.wang@qq.com>
      * @date   2020/8/4 16:11
      */
-    public static function executeSql()
+    public static function executeSql():string
     {
         return self::PHP_EXECUTE_SQL;
     }
